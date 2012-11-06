5f84a84e2cec67d74fb76180cd8adbf028b37cb6
Georgi Georgiev <georgi.hristov@clouway.com>
2012-11-06 10:53:16 +0200

    Refactored a lot of the two tests i think naming is quite nice and everything is readable.

--- test/com/clouway/task1/RemoteRepositoryDataDescribingQueryTest.java
+++ test/com/clouway/task1/RemoteRepositoryDataDescribingQueryTest.java
@@ -1,31 +1,73 @@
 package com.clouway.task1;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author georgi.hristov@clouway.com
  */
 public class RemoteRepositoryDataDescribingQueryTest {
 
+  private Boolean tableExistence = false;
+  private String catalog = null;
+  private String schemaPattern = null;
+  private String tableNamePattern = null;
+  private String[] types = null;
+  private Statement statement;
+  private DatabaseMetaData databaseMetaData;
+  private RemoteRepositoryDataDescribingQuery remoteRepositoryDDQ;
+  private String table = "task1";
+  private String createTableStatement = "CREATE TABLE " + table +
+          "(" +
+          "name varchar(32)," +
+          "age int," +
+          "egn varchar(10) NOT NULL," +
+          "email varchar(32)," +
+          "CONSTRAINT task1_pk PRIMARY KEY(egn)" +
+          ");";
+
+
+  @Before
+  public void setUp() throws SQLException {
+
+    String repositoryAddress = "jdbc:postgresql://localhost:5432/workingdb";
+    String username = "postgres";
+    String password = "";
+    Connection connection = DriverManager.getConnection(repositoryAddress, username, password);
+    databaseMetaData = connection.getMetaData();
+    statement = connection.createStatement();
+    remoteRepositoryDDQ = new RemoteRepositoryDataDescribingQuery(statement);
+    remoteRepositoryDDQ.createTable(createTableStatement);
+  }
+
+
+  @After
+  public void tearDown() throws SQLException {
+
+
+    deleteTable(table);
+
+    statement.close();
+
+  }
+
+
   class RemoteRepositoryDataDescribingQuery {
 
-    private final Connection connection;
     private final Statement statement;
 
-    public RemoteRepositoryDataDescribingQuery(Connection connection, Statement statement) {
-
-      this.connection = connection;
+    public RemoteRepositoryDataDescribingQuery(Statement statement) {
 
       this.statement = statement;
 
@@ -40,7 +82,7 @@ public class RemoteRepositoryDataDescribingQueryTest {
 
     public void dropTable(String tableName) throws SQLException {
 
-      statement.executeUpdate("DROP TABLE " + tableName+ ";");
+      statement.executeUpdate("DROP TABLE " + tableName + ";");
 
     }
 
@@ -49,94 +91,55 @@ public class RemoteRepositoryDataDescribingQueryTest {
 
 
   @Test
+
   public void queryCreatesTable() throws SQLException {
 
-    String repositoryAddress = "jdbc:postgresql://localhost:5432/workingdb";
-    String username = "postgres";
-    String password = "";
-
-    Connection connection = DriverManager.getConnection(repositoryAddress, username, password);
-    Statement statement = connection.createStatement();
-
-    RemoteRepositoryDataDescribingQuery remoteRepositoryDDQ = new RemoteRepositoryDataDescribingQuery(connection, statement);
-
-    String createTableStatement = "CREATE TABLE task1" +
-            "(" +
-            "name varchar(32)," +
-            "age int," +
-            "egn varchar(10) NOT NULL," +
-            "email varchar(32)," +
-            "CONSTRAINT task1_pk PRIMARY KEY(egn)" +
-            ");";
-
-
-    remoteRepositoryDDQ.createTable(createTableStatement);
-
-    ResultSet resultSet = statement.executeQuery("SELECT * FROM task1");
-
-    ResultSetMetaData metaData = resultSet.getMetaData();
-
-    statement.executeUpdate("DROP TABLE task1");
-
-    assertThat(metaData.getColumnCount(), is(4));
-
-    statement.close();
-
-    connection.close();
+    assertThatTableExists();
 
   }
 
 
   @Test
-
   public void queryDropsTable() throws SQLException {
-    String repositoryAddress = "jdbc:postgresql://localhost:5432/workingdb";
-    String username = "postgres";
-    String password = "";
 
-    Connection connection = DriverManager.getConnection(repositoryAddress, username, password);
+    remoteRepositoryDDQ.dropTable(table);
 
-    Statement statement = connection.createStatement();
+    assertThatTableNotExists();
 
-    RemoteRepositoryDataDescribingQuery remoteRepositoryDDQ = new RemoteRepositoryDataDescribingQuery(connection, statement);
-
-    String tablename = "task1";
-
-    String createTableStatement = "CREATE TABLE " + tablename +
-            "(" +
-            "name varchar(32)," +
-            "age int," +
-            "egn varchar(10) NOT NULL," +
-            "email varchar(32)," +
-            "CONSTRAINT task1_pk PRIMARY KEY(egn)" +
-            ");";
-
-    remoteRepositoryDDQ.createTable(createTableStatement);
-    remoteRepositoryDDQ.dropTable(tablename);
+  }
 
 
-    Boolean exist=false;
+  private void assertThatTableExists() throws SQLException {
 
-    DatabaseMetaData databaseMetaData = connection.getMetaData();
+     assertTrue(hasTable(table));
 
-    String   catalog          = null;
-    String   schemaPattern    = null;
-    String   tableNamePattern = null;
-    String[] types            = null;
+  }
 
-    ResultSet result = databaseMetaData.getTables(catalog, schemaPattern, tableNamePattern, types );
+  private void assertThatTableNotExists() throws SQLException {
+
+    assertFalse(hasTable(table));
+
+  }
 
 
+  private boolean hasTable(String tableName) throws SQLException {
 
-    while(result.next()) {
-      String tableName = result.getString(3);
-      if(tableName.equals(tablename)){
-         exist=true;
+    ResultSet result = databaseMetaData.getTables(catalog, schemaPattern, tableNamePattern, types);
+
+    while (result.next()) {
+      String droppedTable = result.getString(3);
+      if (droppedTable.equals(tableName)) {
+        tableExistence = true;
       }
     }
+    return tableExistence;
+  }
 
-    assertThat(exist,is(false));
 
+  private void deleteTable(String tableName) throws SQLException {
+    if (tableExistence) {
+      statement.executeUpdate("DROP TABLE " + tableName + ";");
+    }
   }
 
 }
